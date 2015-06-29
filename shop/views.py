42a84888abffca2ff367eac75fb71767772b314a
Daniel van Flymen <daniel@parcelninja.co.za>
2015-06-29 16:34:30 +0200

    Added Pagination for Products

--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-import datetime
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+
+from . models import Product
 
 
-def
+def index(request):
+    products = Product.objects.all()
+
+    # Let's paginate
+    paginator = Paginator(products, 9)
+    page = request.GET.get('page')
+
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    context_dict = {
+        'page_title': "Products",
+        'page_sub_title': "",
+        'products': products,
+        'navbar': "products"
+    }
+
+    return render(request, 'products/products.html', context_dict)
